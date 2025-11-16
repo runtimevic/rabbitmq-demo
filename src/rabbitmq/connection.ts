853d62d660a41cef2301be575fb01b3059deb529
Víctor Durán <victorduranm@hotmail.com>
2025-11-16 22:54:48 +0100

    Refactor code structure for improved readability and maintainability

--- src/rabbitmq/connection.ts
+++ src/rabbitmq/connection.ts
@@ -2,8 +2,13 @@ import client, { Channel, ChannelModel, Connection } from "amqplib";
 
 let connection: Connection | ChannelModel | null = null;
 let channel: Channel | null = null;
+
+process.loadEnvFile();
+
 const RABBITMQ_URL =
   process.env.RABBITMQ_URL || "amqp://user:root@localhost:5672";
+
+console.log('RABBITMQ_URL=', process.env.RABBITMQ_URL);  
 
 export async function getChannel(): Promise<Channel> {
   if (!connection) {
